a6a9b8a4e007b3e0a1e0bb347da8d7f9b742ccef
franco-maioli03 <franco.maioli03@gmail.com>
2024-12-12 16:56:53 -0300

    Adding CSS and JS code

--- rpsweb/main.go
+++ rpsweb/main.go
@@ -10,6 +10,11 @@ func main() {
 	// Crear enrutador
 	router := http.NewServeMux()
 
+	// Manejador para servir los archivos estaticos
+	fs := http.FileServer(http.Dir("static"))
+	// Ruta para acceder a los archivos estaticos
+	router.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	// Configurar rutas
 	router.HandleFunc("/", handlers.Index)
 	router.HandleFunc("/new", handlers.NewGame)
