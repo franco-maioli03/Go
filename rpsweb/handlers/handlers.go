fc159c030899349687bf2987f34474493cf92df2
franco-maioli03 <franco.maioli03@gmail.com>
2024-12-12 16:24:08 -0300

    ntroduction to the new web project

---
+++ rpsweb/handlers/handlers.go
@@ -0,0 +1,44 @@
+package handlers
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+const (
+	templateDir  = "templates/"
+	templateBase = templateDir + "base.html"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html", nil)
+}
+
+func NewGame(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "new-game.html", nil)
+}
+
+func Game(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "game.html", nil)
+}
+
+func Play(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Jugar")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.html", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, page string, data any) {
+	tpl := template.Must(template.ParseFiles(templateBase, templateDir+page))
+
+	err := tpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, "Error al renderizar la plantilla", http.StatusInternalServerError)
+		log.Println(err)
+		return
+	}
+}
---
+++ rpsweb/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"rpsweb/handlers"
+)
+
+func main() {
+	// Crear enrutador
+	router := http.NewServeMux()
+
+	// Configurar rutas
+	router.HandleFunc("/", handlers.Index)
+	router.HandleFunc("/new", handlers.NewGame)
+	router.HandleFunc("/game", handlers.Game)
+	router.HandleFunc("/play", handlers.Play)
+	router.HandleFunc("/about", handlers.About)
+
+	port := ":8080"
+	log.Printf("Servidor escuchando en http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
